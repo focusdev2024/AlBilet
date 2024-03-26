bd9952535f30b859370268d6bf085ae4ecb7bdea
Cyber Security 202318 <202318@tmhi.edu>
2024-03-26 17:54:17 +0500

    already

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:online_library/pages/read_and_buy_page/presentation/read_and_buy
 import 'package:online_library/pages/sing_up_page/presentation/sing_up_page.dart';
 import 'package:online_library/pages/welcome_page/presentation/welcome_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:online_library/tools/colors/albilet_colors.dart';
 import 'package:online_library/widgets/home_page_details/home_page_details_widget.dart';
 import 'pages/sing_in_page/presentation/login_page.dart';
 
@@ -41,25 +42,24 @@ class OnlineLibrary extends StatelessWidget {
         });
       },
       theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromRGBO(249, 238, 240, 1),
-          primaryColorLight: const Color.fromRGBO(13, 57, 52, 1),
-          primaryColorDark: Colors.white,
-          textTheme: const TextTheme(
-              titleLarge: TextStyle(color: Color.fromRGBO(13, 57, 52, 1)),
-              titleMedium: TextStyle(color: Color.fromRGBO(13, 57, 52, 1))),
+          scaffoldBackgroundColor: AppColors.white,
+          primaryColorLight: AppColors.mainBlue,
+          primaryColorDark: AppColors.white,
+          textTheme: TextTheme(
+              titleLarge: TextStyle(color: AppColors.mainBlue),
+              titleMedium: TextStyle(color: AppColors.mainBlue)),
           shadowColor: Colors.grey.withOpacity(0.2),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color.fromRGBO(13, 57, 52, 1),
-            titleTextStyle: TextStyle(color: Color.fromRGBO(249, 238, 240, 1)),
+          appBarTheme: AppBarTheme(
+            backgroundColor: AppColors.mainBlue,
             iconTheme: IconThemeData(
-              color: Color.fromRGBO(249, 238, 240, 1),
+              color: AppColors.white,
             ),
             elevation: 2.0,
           ),
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Colors.white,
-            selectedItemColor: Color.fromRGBO(13, 57, 52, 1),
-            unselectedItemColor: Color.fromRGBO(13, 57, 52, 1),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: AppColors.mainBlue,
+            selectedItemColor: AppColors.mainYellow,
+            unselectedItemColor: AppColors.mainYellow,
           )),
 
       // home: WelcomePage(),
--- lib/pages/library_main_page/presentation/library_main_page.dart
+++ lib/pages/library_main_page/presentation/library_main_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:online_library/tools/colors/albilet_colors.dart';
 import 'package:online_library/widgets/favorites_list/favorites_list_widget.dart';
 import 'package:online_library/widgets/home_page_details/home_page_details_widget.dart';
 import 'package:online_library/widgets/left_nav_bar.dart';
@@ -24,28 +26,83 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
 
   bool _isSearch = false;
 
+  final List<String> items = ['TM', 'RU', 'EN'];
+  String selectedItem = 'TM';
+  final List<String> places = ['Aşgabat', 'Türkmenistan', 'EN'];
+  String selectedPlace = 'Aşgabat';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const LeftNavBarWidget(),
+      // drawer: const LeftNavBarWidget(),
       appBar: AppBar(
-        title: _isSearch
-            ? _buildSearchField()
-            : Text(
-                AppLocalizations.of(context)!.tagamlySozler,
-              ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(_isSearch ? Icons.close : Icons.search),
-            onPressed: () {
-              setState(() {
-                _isSearch = !_isSearch;
-              });
-            },
+          elevation: 2,
+          // title: _isSearch
+          //     ? _buildSearchField()
+          //     : Text(
+          //         AppLocalizations.of(context)!.tagamlySozler,
+          //       ),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/images/al_bilet001.png',
+                )),
           ),
-        ],
-      ),
+          centerTitle: true,
+          actions: [
+            Row(
+              children: [
+                Icon(
+                  Icons.language,
+                  color: AppColors.mainYellow,
+                ),
+                const SizedBox(width: 5),
+                DropdownButton<String>(
+                  iconDisabledColor: AppColors.mainYellow,
+                  value: selectedItem,
+                  onChanged: (String? newValue) {
+                    if (newValue != null) {
+                      selectedItem = newValue;
+                      // Call setState to update the UI
+                    }
+                  },
+                  items: items.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(color: AppColors.mainYellow),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('welcome');
+                  },
+                  child: Text(
+                    "Log in",
+                    style: TextStyle(color: AppColors.white),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          AppColors.mainYellow)),
+                ),
+                const SizedBox(width: 10),
+                // IconButton(
+                //   icon: Icon(_isSearch ? Icons.close : Icons.search),
+                //   onPressed: () {
+                //     setState(() {
+                //       _isSearch = !_isSearch;
+                //     });
+                //   },
+                // ),],)
+              ],
+            ),
+          ]),
       body: SafeArea(
         child: IndexedStack(
           index: _selectedPage,
@@ -53,6 +110,7 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
             MainDatailsWidget(),
             FavoritesListWidget(),
             const Text('3'),
+            const Text('4'),
           ],
         ),
       ),
@@ -67,12 +125,9 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
           child: GNav(
-              rippleColor: Colors.grey, // tab button ripple color when pressed
-              hoverColor: Colors.grey, // tab button hover color
-              //haptic: true, // haptic feedback
               tabBorderRadius: 25,
               tabShadow: [
-                BoxShadow(color: Theme.of(context).shadowColor, blurRadius: 2)
+                BoxShadow(color: Theme.of(context).shadowColor, blurRadius: 1)
               ], // tab button shadow
               curve: Curves.decelerate, // tab animation curves
               duration:
@@ -87,17 +142,25 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
               padding: const EdgeInsets.all(15),
               selectedIndex: _selectedPage,
               onTabChange: onSelectedPage,
-              tabs: const [
+              tabs: [
                 GButton(
                   icon: Icons.home,
+                  iconColor: AppColors.mainYellow,
                   text: "Home",
                 ),
                 GButton(
                   icon: Icons.favorite_border,
+                  iconColor: AppColors.mainYellow,
                   text: "Favorites",
                 ),
                 GButton(
+                  icon: Icons.shopping_cart_outlined,
+                  iconColor: AppColors.mainYellow,
+                  text: "Cart",
+                ),
+                GButton(
                   icon: Icons.person,
+                  iconColor: AppColors.mainYellow,
                   text: "Profile",
                 ),
               ]),
